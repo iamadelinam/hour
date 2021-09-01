828a816bb30f7f13c15b5b6aa4176f95dc0e279b
unknown <adelyaka.mitsk@gmail.com>
2021-09-01 14:47:34 +0300

    user_name

--- back-end/mysql.sql
+++ back-end/mysql.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE hoursApp;
 USE hoursApp;
-
 CREATE Table authorization (
   id INT PRIMARY KEY AUTO_INCREMENT,
   user_mail VARCHAR (250) NOT NULL,
@@ -8,20 +7,20 @@ CREATE Table authorization (
   salt VARCHAR (50) NOT NULL,
   token VARCHAR (1000)
 );
-
-
+ALTER TABLE
+  table authorization
+ADD
+  [COLUMN] user_name VARCHAR (250) NOT NULL [AFTER user_mail];
+  
 CREATE Table categories (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  title VARCHAR (250) NOT NULL,
-  authorization_id INT NOT NULL
-);
-
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR (250) NOT NULL,
+    authorization_id INT NOT NULL
+  );
 CREATE Table activities (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  category_id INT NOT NULL,
-  time_stamp DATE NOT NULL,
-  act_minutes INT NOT NULL,
-  authorization_id INT NOT NULL
-);
-
-
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    category_id INT NOT NULL,
+    time_stamp DATE NOT NULL,
+    act_minutes INT NOT NULL,
+    authorization_id INT NOT NULL
+  );
