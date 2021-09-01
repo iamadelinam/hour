229f9f0957446186df37f375fb8460c3e0c50993
unknown <adelyaka.mitsk@gmail.com>
2021-09-01 14:52:11 +0300

    fix

--- back-end/mysql.sql
+++ back-end/mysql.sql
@@ -1,18 +1,14 @@
 CREATE DATABASE hoursApp;
-USE hoursApp;
+USE hoursapp;
 CREATE Table authorization (
   id INT PRIMARY KEY AUTO_INCREMENT,
   user_mail VARCHAR (250) NOT NULL,
+  user_name VARCHAR(250) NOT NULL,
   hash VARCHAR (100) NOT NULL,
   salt VARCHAR (50) NOT NULL,
   token VARCHAR (1000)
 );
-ALTER TABLE
-  table authorization
-ADD
-  user_name VARCHAR(250) NOT NULL
-AFTER
-  user_mail;
+
 CREATE Table categories (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR (250) NOT NULL,
