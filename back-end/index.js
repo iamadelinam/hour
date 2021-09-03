6d60a9d8e20dc52cfd71a68e79431570074a584d
Nikita Grishin <mgrin@MacBook-Pro-Nikita.local>
2021-09-03 13:24:52 +0300

    change port

--- back-end/index.js
+++ back-end/index.js
@@ -189,6 +189,6 @@ app.get("/categories/:categoryId", authorizationMiddleware, (req, res) => {
     });
 });
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log("🚀 This server is running on port 3333");
 });
